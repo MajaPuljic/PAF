34ba6140742dfcaa7eb1256563e485bffc0243cf
MajaPuljic <puljicmaja198@gmail.com>
2022-05-27 11:21:00 +0200

    Vjezbe10

---
+++ Vjezbe10/Particle.py
@@ -0,0 +1,94 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Particle:
+
+    def __init__(self,E,B,v0,q,m):
+        self.E = E
+        self.B = B
+        self.v0 = v0
+        self.q = q
+        self.m = m
+        self.a = np.array([0,0,0])
+        self.v = v0
+        self.x = np.array([0,0,0])
+        self.t = 0
+        self.xlist = [0]
+        self.ylist = [0]
+        self.zlist = [0]
+    
+
+    def __FE(self):
+        E = self.E(self.t)
+        Fe = self.q * E
+        return Fe
+
+    def __FB(self):
+        B = self.B(self.t)
+        Fm = self.q * (np.cross(self.v,B))
+        return Fm
+
+    def __move(self,dt):
+        F = self.__FE() + self.__FB()
+        self.a = F / self.m
+        self.v = self.v + self.a * dt
+        self.x = self.x + self.v * dt
+        self.t = self.t + dt
+        self.xlist.append(self.x[0])
+        self.ylist.append(self.x[1])
+        self.zlist.append(self.x[2])
+
+    def __move_RK(self,dt):
+        v0 = self.v
+        x0 = self.x
+
+        F = self.__FE() + self.__FB()
+        self.a = F / self.m
+        k1v = self.a * dt
+        k1x = v0 * dt
+        
+        self.v = v0 + 0.5 * k1v
+        self.x = x0 + 0.5 * k1x
+        F = self.__FE() + self.__FB()
+        self.a = F / self.m
+        k2v = self.a * dt
+        k2x = (v0 + 0.5 * k1v) * dt
+
+        self.v = v0 + 0.5 * k2v
+        self.x = x0 + 0.5 * k2x
+        F = self.__FE() + self.__FB()
+        self.a = F / self.m
+        k3v = self.a * dt
+        k3x = (v0 + 0.5 * k2v) * dt
+
+        self.v = v0 + k3v
+        self.x = x0 + k3x
+        F = self.__FE() + self.__FB()
+        self.a = F / self.m
+        k4v = self.a * dt
+        k4x = (v0 + k3v) * dt
+
+        self.v = v0 + (1/6)*(k1v + 2 * k2v + 2 * k3v + k4v)
+        self.x = x0 + (1/6)*(k1x + 2 * k2x + 2 * k3x + k4x)
+        self.t = self.t + dt
+        
+
+        
+        self.xlist.append(self.x[0])
+        self.ylist.append(self.x[1])
+        self.zlist.append(self.x[2])
+
+    def plot_trajectory(self,dt,T):
+        t = 0
+        while t < T:
+            self.__move(dt)
+            t = t + dt
+
+    def plot_trajectoryRK(self,dt,T):
+        t = 0
+        while t < T:
+            self.__move_RK(dt)
+            t = t + dt
+
+    def liste(self):
+        return self.xlist, self.ylist, self.zlist
---
+++ Vjezbe10/Zadatak1.py
@@ -0,0 +1,49 @@
+import Particle as prt
+import matplotlib.pyplot as plt
+import numpy as np
+
+def E1(t):
+    E1 = np.array([0,0,0])
+    return E1
+
+def B1(t):
+    B1 = np.array([0,0,1])
+    return B1
+
+v01 = np.array([0.1,0.1,0.1])
+q1 = -1
+m1 = 1
+
+p1 = prt.Particle(E1,B1,v01,q1,m1)
+p1.plot_trajectoryRK(0.01,20)
+xlist1, ylist1, zlist1 = p1.liste()
+
+def E2(t):
+    E4 = np.array([0,0,0])
+    return E4
+
+def B2(t):
+    Bz = t/10
+    B4 = np.array([0,0,1])
+    return B4
+
+v02 = np.array([0.1,0.1,0.1])
+q2 = 1
+m2 = 1
+
+p2 = prt.Particle(E2,B2,v02,q2,m2)
+p2.plot_trajectoryRK(0.01,20)
+xlist2, ylist2, zlist2 = p2.liste()
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot3D(xlist1, ylist1, zlist1, c="blue", label="electron ")
+ax.plot3D(xlist2, ylist2, zlist2, c="green", label="positron")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("z")
+ax.view_init(30,30)
+plt.legend()
+plt.show()
+
+
