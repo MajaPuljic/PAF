6b1fca9430944a526a66d9f58b794edacf314884
MajaPuljic <puljicmaja198@gmail.com>
2022-05-27 11:51:08 +0200

    Vjezbe1

--- Vjezbe1/Zadatak5.py
+++ Vjezbe1/Zadatak5.py
@@ -3,30 +3,28 @@ import math
 import matplotlib.pyplot as plt
 
 
-ime_xosi = input("Unesite naziv x osi: ")
-ime_yosi = input("Unesite naziv y osi: ")
-
-
 def pravac(x1,y1,x2,y2):
 
     a = round((y2 - y1) / (x2 -x1),2)
     b = round((y1 - (a*x1)),2)
-
-    x_values = np.linspace(x1,x2,100)
-    y_values = np.linspace(y1,y2,100)
-
-    plt.xlabel(ime_xosi)
-    plt.ylabel(ime_yosi)
+    dx = x2-x1
+    dy = y2-y1
 
     izbor1 = input ("Unesite 1 za prikaz grafa,\n Unesite 2 za spremanje grafa u obliku PDFa: ")
     izbor = int(izbor1)
     ime = input("Unesite ime grafa: ")
 
     if izbor == 1:
-        slika = plt.plot(x_values,y_values)
+        slika = plt.plot([x1-2*dx,x2+2*dx], [y1-2*dy,y2+2*dy])
+        plt.plot(x1,y1,'r*')
+        plt.plot(x2,y2,'r*')
+        plt.xlabel("x")
+        plt.ylabel("y")
         plt.show()
     elif izbor == 2:
-        slika = plt.plot(x_values,y_values) 
+        slika = plt.plot([x1-2*dx,x2+2*dx], [y1-2*dy,y2+2*dy])
+        plt.plot(x1,y1,'r*')
+        plt.plot(x2,y2,'r*') 
         plt.savefig(ime)
     else:
         slika = False
